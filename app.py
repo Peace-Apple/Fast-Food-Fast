394049c00ea2ae041367b42fcd0e76231986d4f0
peace <astonreba@gmail.com>
2018-09-16 22:56:30 +0300

    modified app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def index():
     return jsonify({'about':'Welcome to Fast-Food-Fast API'})
 
 #to get all orders
-@app.route('/orders', methods=['GET'])
+@app.route('/orders/', methods=['GET'])
 def get_orders():
     return jsonify({'orders':orders})
 
@@ -19,9 +19,45 @@ def get_one_order(order_ID):
         if item['order_ID'] == order_ID:
             order = {
                 'name': item['name'],
-                'price': item['price']
+                'email': item['email'],
+                'contact': item['contact'],
+                'location': item['location'],
+                'food_item': item['food_item'],
+                'no_of_items': item['no_of_items'],
+                'price': item['price'],
+                'delivery_time': item['delivery_time'],
+                'order_ID': item['order_ID'],
+                'payment': item['payment']
+                
             }
     return jsonify(order)
+#the orders defined
+orders = [
+    {
+        'name': 'Omech',
+        'email': 'omechemma@gmail.com',
+        'contact': '0706346702',
+        'location': 'Lubowa',
+        'food_item': 'Chicken and Chips',
+        'no_of_items':'2',
+        'price': 15000,
+        'delivery_time':'11.30pm',
+        'order_ID': 1010,
+        'payment': 'cash on delivery',
+    },
+    {
+        'name': 'Apple',
+        'email': 'astonreba@gmail.com',
+        'contact': '0771904376',
+        'location': 'Mbuya',
+        'food_item': 'Chicken Luwombo',
+        'no_of_items':'1',
+        'price': 20000,
+        'order_ID': 1011,
+        'payment': 'cash on delivery',
+        
+    }
+]
 
 if __name__=='__main__':
     app.run(debug=True)
