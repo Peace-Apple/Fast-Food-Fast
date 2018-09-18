ad098fb0a6fbb08118b44c4bd15ed7fb26df768d
peace <astonreba@gmail.com>
2018-09-18 15:52:42 +0300

    Add more tests to test_api.py

--- api/tests/test_api.py
+++ api/tests/test_api.py
@@ -83,3 +83,137 @@ class TestFastFoodFast(TestCase):
         self.assertTrue(response_data['status'], 'success')
         self.assertTrue(response_data['data'])
         self.assertEqual(request_data.status_code, 200)
+
+
+    def test_get_order_not_existing(self):
+        self.make_order('Jerry', 'Fish fillet')
+        self.make_order('Deo', 'Grilled chicken')
+        self.make_order('Bill', 'Ice cream')
+        self.make_order('Sandra', 'Nice Biscuits')
+        self.make_order('Santos', 'Popo drink')
+
+        request_data = self.client().get('/api/v1/orders/8/')
+
+        response_data = json.loads(request_data.data.decode())
+        self.assertTrue(response_data['status'], 'fail')
+        self.assertTrue(response_data['error_message'], 'Order does not exist')
+        self.assertFalse(response_data['data'])
+        self.assertEqual(request_data.status_code, 400)
+
+    def test_get_order_that_exists(self):
+        self.make_order('Jerry', 'Fish fillet')
+        self.make_order('Deo', 'Grilled chicken')
+        self.make_order('Bill', 'Ice cream')
+        self.make_order('Sandra', 'Nice Biscuits')
+        self.make_order('Santos', 'Popo drink')
+
+        request_data = self.client().get('/api/v1/orders/2/')
+
+        response_data = json.loads(request_data.data.decode())
+        self.assertTrue(response_data['status'], 'success')
+        self.assertTrue(response_data['message'], 'Order exists')
+        self.assertTrue(response_data['data'])
+        self.assertEqual(request_data.status_code, 200)
+
+    def test_update_order_status(self):
+        self.make_order('Jerry', 'Fish fillet')
+        self.make_order('Deo', 'Grilled chicken')
+        self.make_order('Bill', 'Ice cream')
+        self.make_order('Sandra', 'Nice Biscuits')
+        self.make_order('Santos', 'Popo drink')
+
+        request_data = self.client().put(
+            '/api/v1/orders/3/',
+            data=json.dumps(dict(
+                order_status="Ready"
+            )),
+            content_type='application/json'
+        )
+
+        response_data = json.loads(request_data.data.decode())
+        self.assertTrue(response_data['status'], 'success')
+        self.assertTrue(response_data['message'], 'Status has been updated')
+        self.assertTrue(request_data.content_type, 'application/json')
+        self.assertEqual(request_data.status_code, 202)
+
+    def test_update_order_absent(self):
+        self.make_order('Jerry', 'Fish fillet')
+        self.make_order('Deo', 'Grilled chicken')
+        self.make_order('Bill', 'Ice cream')
+        self.make_order('Sandra', 'Nice Biscuits')
+        self.make_order('Santos', 'Popo drink')
+
+        request_data = self.client().put(
+            '/api/v1/orders/1000/',
+            data=json.dumps(dict(
+                order_status="Ready"
+            )),
+            content_type='application/json'
+        )
+        response_data = json.loads(request_data.data.decode())
+        self.assertTrue(response_data['status'], 'fail')
+        self.assertTrue(response_data['error_message'], 'Order does not exist')
+        self.assertFalse(response_data['data'])
+        self.assertEqual(request_data.status_code, 400)
+
+    def test_integer_feedBack(self):
+        self.make_order('Jerry', 'Fish fillet')
+        self.make_order('Deo', 'Grilled chicken')
+        self.make_order('Bill', 'Ice cream')
+        self.make_order('Sandra', 'Nice Biscuits')
+        self.make_order('Santos', 'Popo drink')
+
+        request_data = self.client().put(
+            '/api/v1/orders/1/',
+            data=json.dumps(dict(
+                order_status=536536
+            )),
+            content_type='application/json'
+        )
+        post_response = json.loads(request_data.data.decode())
+        self.assertTrue(post_response['status'], 'fail')
+        self.assertTrue(post_response['error_message'], 'Please use character strings')
+        self.assertFalse(post_response['data'])
+        self.assertTrue(request_data.content_type, 'application/json')
+        self.assertEqual(request_data.status_code, 400)
+
+    def test_string_of_numbers_feedback(self):
+        self.make_order('Jerry', 'Fish fillet')
+        self.make_order('Deo', 'Grilled chicken')
+        self.make_order('Bill', 'Ice cream')
+        self.make_order('Sandra', 'Nice Biscuits')
+        self.make_order('Santos', 'Popo drink')
+
+        request_data = self.client().put(
+            '/api/v1/orders/1/',
+            data=json.dumps(dict(
+                order_status='536536'
+            )),
+            content_type='application/json'
+        )
+
+        post_response = json.loads(request_data.data.decode())
+        self.assertTrue(post_response['status'], 'fail')
+        self.assertTrue(post_response['error_message'], 'Please use character strings')
+        self.assertFalse(post_response['data'])
+        self.assertTrue(request_data.content_type, 'application/json')
+        self.assertEqual(request_data.status_code, 400)
+
+    def test_get_empty_orders(self):
+        request_data = self.client().get('/api/v1/orders/')
+
+        response_data = json.loads(request_data.data.decode())
+
+        self.assertTrue(response_data['status'], 'success')
+        self.assertTrue(response_data['message'], 'No orders currently')
+        self.assertFalse(response_data['data'])
+        self.assertEqual(request_data.status_code, 200)
+
+    def test_get_order_from_empty_storage(self):
+        request_data = self.client().get('/api/v1/orders/1/')
+
+        response_data = json.loads(request_data.data.decode())
+        self.assertTrue(response_data['status'], 'success')
+        self.assertTrue(response_data['message'], 'No orders currently')
+        self.assertFalse(response_data['data'])
+        self.assertEqual(request_data.status_code, 200)
