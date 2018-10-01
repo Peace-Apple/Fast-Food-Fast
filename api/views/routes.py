c1afe48cbcb4f0ef6e44e09c411d9b7d3ffd9150
peace <astonreba@gmail.com>
2018-10-01 17:44:52 +0300

    Modify routes.py file

--- api/views/routes.py
+++ api/views/routes.py
@@ -16,11 +16,22 @@ class Urls:
         :param app:
         :return:
         """
-        app.add_url_rule('/api/v1/orders/', view_func=OrdersController.as_view('make_order'),
+        app.add_url_rule('/api/v2/auth/signup/', view_func=OrdersController.as_view('register_user'),
                          methods=['POST'], strict_slashes=False)
-        app.add_url_rule('/api/v1/orders/', view_func=OrdersController.as_view('get_orders'),
+        app.add_url_rule('/api/v2/auth/login/', view_func=OrdersController.as_view('login_user'),
+                         methods=['POST'], strict_slashes=False)
+        app.add_url_rule('/api/v2/users/orders/', view_func=OrdersController.as_view('make_order'),
+                         methods=['POST'], strict_slashes=False)
+        app.add_url_rule('/api/v2/users/orders/', view_func=OrdersController.as_view('get_history'),
                          methods=['GET'], strict_slashes=False)
-        app.add_url_rule('/api/v1/orders/<int:order_id>/', view_func=OrdersController.as_view('get_single_order'),
+        app.add_url_rule('/api/v2/orders/', view_func=OrdersController.as_view('get_orders'),
                          methods=['GET'], strict_slashes=False)
-        app.add_url_rule('/api/v1/orders/<int:order_id>/', view_func=OrdersController.as_view('update_order_status'),
+        app.add_url_rule('/api/v2/orders/<int:order_id>/', view_func=OrdersController.as_view('get_single_order'),
+                         methods=['GET'], strict_slashes=False)
+        app.add_url_rule('/api/v2/orders/<int:order_id>/', view_func=OrdersController.as_view('update_order_status'),
                          methods=['PUT'], strict_slashes=False)
+        app.add_url_rule('/api/v2/menu/', view_func=OrdersController.as_view('get_menu'),
+                         methods=['GET'], strict_slashes=False)
+        app.add_url_rule('/api/v2/menu/', view_func=OrdersController.as_view('add_meal'),
+                         methods=['POST'], strict_slashes=False)
+
