6eb4178b58b3c780de0a1d9dbc737edc851692a9
peace <astonreba@gmail.com>
2018-10-02 13:51:59 +0300

    Modify database.py file

--- api/models/database.py
+++ api/models/database.py
@@ -4,19 +4,20 @@ Module to handle data storage
 
 import psycopg2
 
+
 class DatabaseConnection:
 
     def __init__(self):
         try:
-            self.connection = psycopg2.connect(database="fastfoodfast", user="postgres", password="apple123", host="127.0.0.1", port="5432")
+            self.connection = psycopg2.connect(database="fastfoodfast", user="postgres", password="apple123",
+                                               host="127.0.0.1", port="5432")
             self.connection.autocommit = True
-            self.cursor = self.connection.cusor()
+            self.cursor = self.connection.cursor()
             print("Database opened successfully")
 
         except:
             print("Database connection failed")
 
-    @staticmethod
     def create_tables(self):
         """
         This method creates tables one after the other in the database after the connection has been established.
@@ -29,13 +30,13 @@ class DatabaseConnection:
                     email VARCHAR(50) UNIQUE NOT NULL, phone_number INTEGER NOT NULL,
                     user_type BOOLEAN NOT NULL DEFAULT FALSE, password VARCHAR(255) NOT NULL,
                     is_loggedin BOOLEAN DEFAULT FALSE
-                )
+                );
             """,
             """
             CREATE TABLE IF NOT EXISTS "menu" (
                     item_id SERIAL PRIMARY KEY, item_category VARCHAR(50) NOT NULL,
                     item_name VARCHAR(50) NOT NULL, price INTEGER NOT NULL
-                )
+                );
             """,
             """
             CREATE TABLE IF NOT EXISTS "orders" (
@@ -47,27 +48,20 @@ class DatabaseConnection:
                     ON UPDATE CASCADE ON DELETE CASCADE,
                     FOREIGN KEY (item_id) REFERENCES "menu" (item_id)
                     ON UPDATE CASCADE ON DELETE CASCADE
-                )
-            """,)
+                );
+            """)
 
-        self.cursor.execute(commands)
-
-
-
-
-
-
-
-
-
-
-
+        try:
+            for command in commands:
+                self.cursor.execute(command)
+            self.connection.commit()
+            self.cursor.close()
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+        finally:
+            if self.connection is not None:
+                self.connection.close()
 
 
-
-
-
-
-
-
-
+if __name__ == "__main__":
+    DatabaseConnection().create_tables()
