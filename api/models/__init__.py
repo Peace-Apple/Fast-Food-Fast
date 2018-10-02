fd185e35b5223510ea48ccc10adeafeaa9a5e1be
peace <astonreba@gmail.com>
2018-10-02 18:44:42 +0300

    Modify api directory

--- api/models/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from api.models import order_model
-
-
-app = Flask(__name__)
-
-db_obj = order_model.DatabaseConnection()
-
--- api/models/database.py
+++ api/models/database.py
@@ -26,21 +26,27 @@ class DatabaseConnection:
         commands = (
             """
             CREATE TABLE IF NOT EXISTS "users" (
-                    user_id SERIAL PRIMARY KEY, username VARCHAR(25) NOT NULL,
-                    email VARCHAR(50) UNIQUE NOT NULL, phone_number INTEGER NOT NULL,
-                    user_type BOOLEAN NOT NULL DEFAULT FALSE, password VARCHAR(255) NOT NULL,
+                    user_id SERIAL PRIMARY KEY,
+                    username VARCHAR(25) NOT NULL,
+                    email VARCHAR(50) UNIQUE NOT NULL,
+                    phone_number INTEGER NOT NULL,
+                    user_type BOOLEAN NOT NULL DEFAULT FALSE,
+                    password VARCHAR(255) NOT NULL,
                     is_loggedin BOOLEAN DEFAULT FALSE
                 );
             """,
             """
             CREATE TABLE IF NOT EXISTS "menu" (
-                    item_id SERIAL PRIMARY KEY, item_category VARCHAR(50) NOT NULL,
-                    item_name VARCHAR(50) NOT NULL, price INTEGER NOT NULL
+                    item_id SERIAL PRIMARY KEY, 
+                    item_category VARCHAR(50) NOT NULL,
+                    item_name VARCHAR(50) NOT NULL, 
+                    price INTEGER NOT NULL
                 );
             """,
             """
             CREATE TABLE IF NOT EXISTS "orders" (
-                    order_id SERIAL PRIMARY KEY, user_id INTEGER NOT NULL,
+                    order_id SERIAL PRIMARY KEY, 
+                    user_id INTEGER NOT NULL,
                     item_id INTEGER NOT NULL, 
                     order_status VARCHAR(25) NOT NULL DEFAULT 'new',
                     quantity INTEGER NOT NULL,
@@ -63,5 +69,42 @@ class DatabaseConnection:
                 self.connection.close()
 
 
-if __name__ == "__main__":
-    DatabaseConnection().create_tables()
+class TableData:
+
+        def __init__(self):
+            self.connection = psycopg2.connect(database="fastfoodfast", user="postgres", password="apple123",
+                                               host="127.0.0.1", port="5432")
+            self.connection.autocommit = True
+            self.cursor = self.connection.cursor()
+
+        def insert_into_user(self):
+            sql = """INSERT INTO USERS(user_id, username, email, phone_number, user_type, password, is_loggedin) \
+            VALUES('{0}', '{1}', '{2}', '{3}', FALSE, '{5}', FALSE)"""
+            self.connection.commit()
+            self.cursor.execute(sql)
+            return True
+
+
+        def insert_into_menu(self):
+            sql = """INSERT INTO MENU(item_id, item_name, item_category, price)\
+            VALUES('{0}', '{1}', '{2}', '{3}')"""
+            self.connection.commit()
+            self.cursor.execute(sql)
+            return True
+
+
+        def insert_into_orders(self):
+            sql = """INSERT INTO ORDERS(order_id, user_id, item_id, order_status, quantity)\
+                        VALUES('{0}', '{1}', '{2}', new '{4}')"""
+            self.connection.commit()
+            self.cursor.execute(sql)
+            return True
+
+
+
+
+
+
+
+
+
