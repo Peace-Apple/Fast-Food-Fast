b81181584616b08f82a4e9a182e94b63d86a7a75
peace <astonreba@gmail.com>
2018-10-02 12:43:52 +0300

    Modify database.py file

---
+++ database.py
@@ -0,0 +1,73 @@
+"""
+Module to handle data storage
+"""
+
+import psycopg2
+
+class DatabaseConnection:
+
+    def __init__(self):
+        try:
+            self.connection = psycopg2.connect(database="fastfoodfast", user="postgres", password="apple123", host="127.0.0.1", port="5432")
+            self.connection.autocommit = True
+            self.cursor = self.connection.cusor()
+            print("Database opened successfully")
+
+        except:
+            print("Database connection failed")
+
+    @staticmethod
+    def create_tables(self):
+        """
+        This method creates tables one after the other in the database after the connection has been established.
+
+        """
+        commands = (
+            """
+            CREATE TABLE IF NOT EXISTS "users" (
+                    user_id SERIAL PRIMARY KEY, username VARCHAR(25) NOT NULL,
+                    email VARCHAR(50) UNIQUE NOT NULL, phone_number INTEGER NOT NULL,
+                    user_type BOOLEAN NOT NULL DEFAULT FALSE, password VARCHAR(255) NOT NULL,
+                    is_loggedin BOOLEAN DEFAULT FALSE
+                )
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS "menu" (
+                    item_id SERIAL PRIMARY KEY, item_category VARCHAR(50) NOT NULL,
+                    item_name VARCHAR(50) NOT NULL, price INTEGER NOT NULL
+                )
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS "orders" (
+                    order_id SERIAL PRIMARY KEY, user_id INTEGER NOT NULL,
+                    item_id INTEGER NOT NULL, 
+                    order_status VARCHAR(25) NOT NULL DEFAULT 'new',
+                    quantity INTEGER NOT NULL,
+                    FOREIGN KEY (user_id) REFERENCES "users" (user_id)
+                    ON UPDATE CASCADE ON DELETE CASCADE,
+                    FOREIGN KEY (item_id) REFERENCES "menu" (item_id)
+                    ON UPDATE CASCADE ON DELETE CASCADE
+                )
+            """,)
+
+        self.cursor.execute(commands)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
