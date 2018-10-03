d63aba6a13e7a468ff6af575c873d2f73bce880e
peace <astonreba@gmail.com>
2018-10-03 15:15:36 +0300

    Modify order_controllers.py file

--- api/controllers/orders_controllers.py
+++ api/controllers/orders_controllers.py
@@ -1,105 +1,123 @@
 """
-Module to handle any logic
+Module to handle menu logic
 """
 from flask import request, jsonify
 from flask.views import MethodView
 
-from api.models.order_model import ApplicationData
+from api.handlers.response_errors import ResponseErrors
+from api.models.food_model import FoodItems
+from api.models.order_model import Orders
+from api.utils.validation import DataValidation
 
 
 class OrdersController(MethodView):
     """
-    Class with special methods to handle post, put and get requests
+    Class has special methods to handle menu logic
     """
-    ordered_by = None
-    order_items = None
-    order_store = ApplicationData()
-    order_status = None
+
+    order_item = None
+    validate = DataValidation()
+    menu = FoodItems()
+    orders = Orders()
+    quantity = None
 
     def post(self):
         """
-        post method to handle post requests
+        Method to post an order
         :return:
-        """
+         """
+
         post_data = request.get_json()
 
+        key = ('order_item', 'quantity')
+
+        if not set(key).issubset(set(post_data)):
+            return ResponseErrors.missing_fields(key)
+
         try:
-            self.ordered_by = post_data["ordered_by"].strip()
-            self.order_items = post_data["order_items"].strip()
+            self.order_item = post_data['order_item'].strip()
+            self.quantity = post_data['quantity'].strip()
+
         except AttributeError:
             return ResponseErrors.invalid_data_format()
-
-        if not self.ordered_by or not self.order_items:
+        if not self.order_item:
             return ResponseErrors.empty_data_fields()
-        elif DataValidation.check_string_of_numbers(self.ordered_by) or \
-                DataValidation.check_string_of_numbers(self.order_items):
-            return ResponseErrors.invalid_data_format()
-        current_order = self.order_store.make_order(self.ordered_by, self.order_items)
+        elif not self.DataValidation.check_item_name(self.order_item):
+            return ResponseErrors.item_not_on_the_menu(self.order_item.lower())
+
+        order = self.orders.make_order(self.order_item.lower(), self.special_notes)
 
         response_object = {
-            'status': 'Success',
-            'message': 'Your order is submitted',
-            'data': current_order
-        }
+            'status': 'success',
+            'message': 'Successfully posted an order',
+            'data': order.__dict__
+                }
         return jsonify(response_object), 201
 
 
     def get(self, order_id=None):
         """
-        get method to return a list of orders
-        :param order_id:
+        Method to return all existing orders
         :return:
         """
-        if not self.order_store.get_orders():
-            return ResponseErrors.empty_data_storage()
-        elif order_id:
-            return self.get_order(order_id)
 
-        response_object = {
-            'status': 'success',
-            'data': self.order_store.get_orders()
-        }
-        return jsonify(response_object), 200
+        if order_id:
+            return self.get_single_order(order_id)
 
-    def get_order(self, order_id):
-        """
-        method to return a single order
-        :param order_id:
-        :return:
-        """
-        for order in self.order_store.get_orders():
-            if order['order_id'] == order_id:
+        current_orders = self.OrderModel.get_orders()
+
+        if current_orders:
+            if isinstance(current_orders, list) and len(current_orders) > 0:
                 response_object = {
-                    'status': 'success',
-                    'message': 'Order exists',
-                    'data': order
-                }
+                    "status": "success",
+                    "data": [obj.__dict__ for obj in current_orders]
+                        }
                 return jsonify(response_object), 200
+            elif isinstance(current_orders, object):
+                response_object = {
+                    "status": "success",
+                    "data": [current_orders.__dict__]
+                        }
+                return jsonify(response_object), 200
+            else:
+                return ResponseErrors.no_items('order')
 
-        return ResponseErrors.order_absent()
+    def get_single_order(self, order_id):
+        """
+        method to return a specific order
+        :param order_id:
+        :return:
+        """
+        single_order = self.orders.find_order_by_id(order_id)
+        if single_order:
+            response_object = {
+                'status': 'success',
+                'data': single_order.__dict__
+            }
+            return jsonify(response_object), 200
+        return ResponseErrors.no_order()
+
     def put(self, order_id):
         """
-        method to make changes to the order status
-        :param order_id:
+        Method to update an order status
         :return:
         """
-        order = self.order_store.find_one_order(order_id)
-
         post_data = request.get_json()
-        key = 'order_status'
+
+        key = "order_status"
+
+        status = ['new', 'processing', 'cancelled', 'complete']
+
         if key not in post_data:
-            return ResponseErrors.missing_key(key)
+            return ResponseErrors.missing_fields(key)
         try:
-            self.order_status = post_data['order_status'].strip()
+            order_status = post_data['order_status'].strip()
         except AttributeError:
             return ResponseErrors.invalid_data_format()
+        if order_status.lower() not in status:
+            return ResponseErrors.order_status_not_found(order_status)
+        if self.orders.find_order_by_id(order_id):
+            return self.orders.update_order(order_id, order_status)
+        return ResponseErrors.no_items('order')
 
-        if not self.order_status:
-            return ResponseErrors.empty_data_fields()
-        elif DataValidation.check_string_of_numbers(self.order_status):
-            return ResponseErrors.invalid_data_format()
 
-        if not order:
-            return ResponseErrors.order_absent()
-
-        return self.order_store.update_order(order_id, self.order_status)
