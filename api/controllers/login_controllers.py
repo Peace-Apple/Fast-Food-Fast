499dd1116a59df6851cfbb1ce1e6317f8d5fb6e6
peace <astonreba@gmail.com>
2018-10-03 15:15:00 +0300

    Modify login_controllers.py file

--- api/controllers/login_controllers.py
+++ api/controllers/login_controllers.py
@@ -9,6 +9,8 @@ from api.models.order_model import OrderModel
 from api.models.user_model import Users
 from api.utils.validation import DataValidation
 from api.handlers.response_errors import  ResponseErrors
+from api.models.database import DatabaseConnection
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
 
 
 
@@ -37,14 +39,14 @@ class LoginControl(MethodView):
         if not user_name or not password:
             return ResponseErrors.empty_data_fields()
 
-        user = self.user.find_user_by_username(user_name)
+        user_id = self.myUser.find_user_by_username(user_name)
+        password_hash = self.user_model.Users.generate_hash(password)
+        if user_id and password_hash:
 
-        if user and self.auth.verify_password(password, user.password):
-            auth_token = self.auth.encode_auth_token(user.user_id)
             response_object = {
                 'status': 'success',
                 'message': 'You have logged in',
-                'auth_token': auth_token.decode(),
+                "access_token": create_access_token(identity=user_id),
                 'logged_in_as': user.user_type
             }
             return jsonify(response_object), 200
@@ -55,37 +57,3 @@ class LoginControl(MethodView):
             }
             return jsonify(response_object), 404
 
-    def get(self):
-        """
-        Method to return the order history of a user
-        :return:
-        """
-
-        # get auth_token
-        auth_header = request.headers.get('Authorization')
-        if self.validate.check_auth_header(auth_header):
-            auth_token = self.validate.check_auth_header(auth_header)
-
-            resp = self.auth.decode_auth_token(auth_token)
-            if not isinstance(resp, str):
-                order_history = self.orders.order_history(resp)
-                if order_history:
-                    if isinstance(order_history, list):
-                        response_object = {
-                            "status": "success",
-                            "data": [obj.__dict__ for obj in order_history]
-                        }
-                        return jsonify(response_object), 200
-                    elif isinstance(order_history, object):
-
-                        response_object = {
-                            "status": "success",
-                            "data": [order_history.__dict__]
-                        }
-                        return jsonify(response_object), 200
-                else:
-                    return ReturnError.no_items('order')
-            else:
-                return ReturnError.invalid_user_token(resp)
-        else:
-            return ReturnError.user_bearer_token_error()
