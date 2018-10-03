7b1f295d4de49bcc1b3f251517e91ef3666efc2a
peace <astonreba@gmail.com>
2018-10-03 03:12:32 +0300

    Import some modules within the routes.py file

--- api/views/routes.py
+++ api/views/routes.py
@@ -1,6 +1,7 @@
 """
 Routes module to handle request urls
 """
+from api.controllers.login_controllers import LoginControl
 from api.controllers.orders_controllers import OrdersController
 
 
@@ -16,9 +17,9 @@ class Urls:
         :param app:
         :return:
         """
-        app.add_url_rule('/api/v2/auth/signup/', view_func=OrdersController.as_view('register_user'),
+        app.add_url_rule('/api/v2/auth/signup/', view_func=LoginControl.as_view('register_user'),
                          methods=['POST'], strict_slashes=False)
-        app.add_url_rule('/api/v2/auth/login/', view_func=OrdersController.as_view('login_user'),
+        app.add_url_rule('/api/v2/auth/login/', view_func=LoginControl.as_view('login_user'),
                          methods=['POST'], strict_slashes=False)
         app.add_url_rule('/api/v2/users/orders/', view_func=OrdersController.as_view('make_order'),
                          methods=['POST'], strict_slashes=False)
