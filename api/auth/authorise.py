5b478fd36b78f95496ddb2c4f0130191a7631962
peace <astonreba@gmail.com>
2018-10-04 19:25:10 +0300

    Add auth directory that contains a class to handle authentication

---
+++ api/auth/authorise.py
@@ -0,0 +1,78 @@
+"""
+Authentication module for JWT token
+"""
+import datetime
+
+import bcrypt
+import jwt
+
+from api.config.config import Config
+from api.models.user_model import Users
+from werkzeug.security import generate_password_hash
+
+class Authenticate:
+    """
+    Class defines method used by JWT
+    """
+    myUser = Users()
+
+    @staticmethod
+    def encode_auth_token(user_id):
+        """
+        Generate auth token
+        :param user_id:
+        :return:
+        """
+        try:
+            payload = {
+                'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=3600),
+                'iat': datetime.datetime.utcnow(),
+                'sub': user_id
+            }
+            val = jwt.encode(payload, Config.SECRET_KEY, algorithm='HS256')
+            return val
+        except Exception as ex:
+            return ex
+
+    @staticmethod
+    def decode_auth_token(auth_token):
+        """
+        Decodes the auth token
+        :param auth_token:
+        :return:
+        """
+
+        try:
+            payload = jwt.decode(auth_token, Config.SECRET_KEY)
+
+            return payload['sub']
+        except jwt.ExpiredSignatureError:
+            return 'Signature expired. Please log in again.'
+        except jwt.InvalidTokenError:
+            return 'Invalid token. Please log in again.'
+
+    @staticmethod
+    @staticmethod
+    def hash_password(password):
+        """
+        method to hash password
+        :param password:
+        :return:
+        """
+        try:
+            return generate_password_hash(password, method='sha256')
+        except ValueError:
+            return False
+
+    @staticmethod
+    def verify_password(password_text, hashed):
+        """
+        verify client password with stored password
+        :param password_text:
+        :param hashed:
+        :return:
+        """
+        try:
+            return bcrypt.checkpw(password_text.encode('utf8'), hashed)
+        except ValueError:
+            return False
