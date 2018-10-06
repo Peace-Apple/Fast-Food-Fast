7bbe44eb3b9959f7dc4e1b4bc9dc033bc2ca4bef
peace <astonreba@gmail.com>
2018-10-06 16:53:59 +0300

    Modify run.py file to include a decorator that is used to set the value of admin to true in the database

--- run.py
+++ run.py
@@ -3,27 +3,27 @@ app root of the api endpoints. this module runs the application
 """
 
 from flask import Flask
-import api.config.config
 from api.views.routes import Urls
+from api.models.database import DatabaseConnection
+from flask_jwt_extended import JWTManager
+
+app = Flask(__name__)
+app.env = 'development'
+Urls.generate(app)
+app.config['JWT_SECRET_KEY'] = 'pass123'
+jwt = JWTManager(app)
 
 
-class Server:
-    """Create flask object to start the server"""
+@app.before_first_request
+def admin():
+    my_data = DatabaseConnection()
+    my_data.check_admin()
 
-    @staticmethod
-    def create_app(config=None):
-        """
-        Method create a flask object
-        :param config: None
-        :return: app
-        """
-        app = Flask(__name__)
-        app.config.update(config.__dict__ or {})
-        Urls.generate(app)
-        return app
-
-
-APP = Server().create_app(config=api.config.config.DevelopmentConfig)
 
 if __name__ == '__main__':
-    APP.run()
+    app.run(debug=True)
+
+
+
+
+
