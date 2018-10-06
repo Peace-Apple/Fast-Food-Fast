09d14bf28fd89c45aa9bdc145292010d76951cd5
peace <astonreba@gmail.com>
2018-10-06 16:56:36 +0300

    Modify user_model.py file to exclude a user from adding his user_type when registering

--- api/models/user_model.py
+++ api/models/user_model.py
@@ -4,25 +4,26 @@ Module for the user
 
 from api.models.database import DatabaseConnection
 
+
 class UsersModel:
     """
     Model to hold user data
     """
 
-    def __init__(self, user_name=None, email=None, phone_number=None, password=None, user_type=None):
+    def __init__(self, user_name=None, email=None, phone_number=None, password=None):
 
         """
         User model template
-        :rtype: int
         :param user_name:
         :param email:
+        :param phone_number:
         :param password:
         """
         self.user_name = user_name
         self.email = email
         self.password = password
         self.phone_number = phone_number
-        self.user_type = user_type
+        self.user_type = None
         self.user_id = None
 
 
@@ -30,27 +31,23 @@ class Users:
     """
     Define user module attributes accessed by callers
     """
-    _database_ = DatabaseConnection()
+    my_data = DatabaseConnection()
 
-
-    def register_user(self, user_name=None, email=None, phone_number=None,
-                      password=None, user_type=None):
+    def register_user(self, user_name=None, email=None, phone_number=None, password=None):
         """
         Register new user
         :param user_name:
         :param email:
         :param phone_number:
         :param password:
-        :param user_type:
         :return:
         """
-        user = UsersModel(user_name, email, phone_number, password, user_type)
-        self._database_.insert_user(user_name, email, phone_number, password, user_type)
+        user = UsersModel(user_name, email, phone_number, password)
+        self.my_data.insert_user(user_name, email, phone_number, password)
 
         del user.user_id
 
         return user
-
 
 
 
