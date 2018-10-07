089fca1ab4b401af9df6b6887f1b80e261855628
peace <astonreba@gmail.com>
2018-10-07 20:10:12 +0300

    Modify post and get methods in this file

--- api/controllers/orders_controllers.py
+++ api/controllers/orders_controllers.py
@@ -3,6 +3,8 @@ Module to handle order logic
 """
 from flask import request, jsonify
 from flask.views import MethodView
+from flask_jwt_extended import jwt_required, get_jwt_identity
+
 from api.handlers.response_errors import ResponseErrors
 from api.models.food_model import FoodItems
 from api.models.order_model import OrderModel
@@ -18,159 +20,141 @@ class OrdersController(MethodView):
     order_item = None
     quantity = None
     val = DataValidation()
-    menu = FoodItems()
+    food = FoodItems()
     orders = OrderModel()
-    myorder = DatabaseConnection()
+    data = DatabaseConnection()
 
-
+    @jwt_required
     def post(self):
         """
         Method to post an order
         :return:
          """
+        user = get_jwt_identity()
+        user_type = user[4]
+        user_id = user[0]
 
-        # get auth_token
-        auth_header = request.headers.get('Authorization')
+        if user_type != "TRUE" and user_id:
 
-        if self.validate.check_auth_header(auth_header):
-            auth_token = self.validate.check_auth_header(auth_header)
+            post_data = request.get_json()
 
-            resp = self.auth.decode_auth_token(auth_token)
+            key = ('order_item', 'quantity')
 
-            if not isinstance(resp, str):
-                post_data = request.get_json()
+            if not set(key).issubset(set(post_data)):
+                return ResponseErrors.missing_fields(key)
 
-                key = ('order_item', 'quantity')
+            try:
+                self.order_item = post_data['order_item'].strip()
+                self.quantity = post_data['quantity'].strip()
+            except AttributeError:
+                return ResponseErrors.invalid_data_format()
+            if not self.order_item or not self.quantity:
+                return ResponseErrors.empty_data_fields()
+            elif not self.val.check_item_name(self.order_item):
+                return ResponseErrors.item_not_on_the_menu(self.order_item.lower())
 
-                if not set(key).issubset(set(post_data)):
-                    return ResponseErrors.missing_fields(key)
+            order = self.orders.make_order(self.order_item.lower(), self.quantity, str(user_id), str(item_id))
 
-                try:
-                    self.order_item = post_data['order_item'].strip()
-                    self.quantity = post_data['quantity'].strip()
-                except AttributeError:
-                    return ResponseErrors.invalid_data_format()
-                if not self.quantity:
-                    self.quantity = "No quauntity specified"
-
-                if not self.order_item:
-                    return ResponseErrors.empty_data_fields()
-                elif not self.validate.check_item_name(self.order_item):
-                    return ResponseErrors.item_not_on_the_menu(self.order_item.lower())
-
-                order = self.orders.make_order(resp, self.order_item.lower(), self.quantity)
-
-                response_object = {
-                    'status': 'success',
-                    'message': 'Successfully posted an order',
-                    'data': order.__dict__
+            response_object = {
+                'status': '201',
+                'message': 'Successfully posted an order',
+                'data': order
                 }
-                return jsonify(response_object), 201
-
-            else:
-                return ResponseErrors.invalid_user_token(resp)
+            return jsonify(response_object)
         else:
-            return ResponseErrors.user_bearer_token_error()
+            return ResponseErrors.denied_permission()
 
-    def get(self, order_id=None):
+    @jwt_required
+    def get(self):
         """
         Method to return all existing orders
         :return:
         """
+        user = get_jwt_identity()
+        user_type = user[4]
+        user_id = user[0]
 
-        # get auth_token
-        auth_header = request.headers.get('Authorization')
-        if self.val.check_auth_header(auth_header):
-            auth_token = self.val.check_auth_header(auth_header)
+        if user_type == "TRUE" and user_id:
 
-            resp = self.auth.decode_auth_token(auth_token)
+            current_orders = self.data.get_all_orders()
 
-            if not isinstance(resp, str):
+            if isinstance(current_orders, object):
 
-                if self.val.check_user_type(resp):
-
-                    if order_id:
-                        return self.get_single_order(order_id)
-
-                    current_orders = self.orders.get_orders()
-
-                    if current_orders:
-                        if isinstance(current_orders, list) and len(current_orders) > 0:
-                            response_object = {
-                                "status": "success",
-                                "data": [obj.__dict__ for obj in current_orders]
-                            }
-                            return jsonify(response_object), 200
-                        elif isinstance(current_orders, object):
-
-                            response_object = {
-                                "status": "success",
-                                "data": [current_orders.__dict__]
-                            }
-                            return jsonify(response_object), 200
-                    else:
-                        return ResponseErrors.no_items('order')
-
-                return ResponseErrors.denied_permission()
+                response_object = {
+                    "status": "200",
+                    "msg": "success",
+                    "data": current_orders
+                    }
+                return jsonify(response_object)
             else:
-                return ResponseErrors.invalid_user_token(resp)
-        else:
-            return ResponseErrors.user_bearer_token_error()
+                return ResponseErrors.no_items('order')
 
+        return ResponseErrors.denied_permission()
+
+    @jwt_required
     def get_single_order(self, order_id):
         """
         method to return a specific order
         :param order_id:
         :return:
         """
-        single_order = self.myorder.find_order_by_id(order_id)
-        if single_order:
-            response_object = {
-                'status': 'success',
-                'data': single_order.__dict__
-            }
-            return jsonify(response_object), 200
-        return ResponseErrors.no_order()
+        user = get_jwt_identity()
+        user_type = user[4]
+        user_id = user[0]
 
+        if user_type == "TRUE" and user_id:
+
+            single_order = self.data.get_a_specific_order(order_id)
+            if single_order:
+                response_object = {
+                    'status': '200',
+                    'msg': 'success',
+                    'data': single_order.__dict__
+                }
+                return jsonify(response_object)
+            return ResponseErrors.no_order()
+
+    @jwt_required
     def put(self, order_id):
         """
         Method to update an order status
         :return:
         """
+        user = get_jwt_identity()
+        user_type = user[4]
+        user_id = user[0]
 
-        # get auth_token
-        auth_header = request.headers.get('Authorization')
-        if self.val.check_auth_header(auth_header):
-            auth_token = self.val.check_auth_header(auth_header)
+        if user_type == "TRUE" and user_id:
 
-            resp = self.auth.decode_auth_token(auth_token)
+            post_data = request.get_json()
 
-            if not isinstance(resp, str):
-                if self.val.check_user_type(resp):
-                    post_data = request.get_json()
+            key = "order_status"
 
-                    key = "order_status"
+            status = ['new', 'processing', 'cancelled', 'completed']
 
-                    status = ['new', 'processing', 'cancelled', 'completed']
+            if key not in post_data:
+                return ResponseErrors.missing_fields(key)
+            try:
+                order_status = post_data['order_status'].strip()
+            except AttributeError:
+                return ResponseErrors.invalid_data_format()
 
-                    if key not in post_data:
-                        return ResponseErrors.missing_fields(key)
-                    try:
-                        order_status = post_data['order_status'].strip()
-                    except AttributeError:
-                        return ResponseErrors.invalid_data_format()
+            if order_status.lower() not in status:
+                return ResponseErrors.order_status_not_found(order_status)
+            if self.data.find_order_by_id(order_id):
 
-                    if order_status.lower() not in status:
-                        return ResponseErrors.order_status_not_found(order_status)
-                    if self.myorder.find_order_by_id(order_id):
-                        return self.orders.update_order(order_id, order_status)
-                    return ResponseErrors.no_items('order')
+                updated_order = self.data.update_order(order_id, order_status)
+                if updated_order:
+                    response_object = {
+                        'status': '202',
+                        'message': 'Status has been updated successfully'
+                    }
+                    return jsonify(response_object)
 
-                return ResponseErrors.denied_permission()
-            else:
-                return ResponseErrors.invalid_user_token(resp)
-        else:
-            return ResponseErrors.user_bearer_token_error()
+            return ResponseErrors.no_items('order')
+
+        return ResponseErrors.denied_permission()
+
 
 
 
