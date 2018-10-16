06667955807266bbb73102bbf26c369419217471
peace <astonreba@gmail.com>
2018-10-16 20:40:02 +0300

    Add some comments

--- api/models/database.py
+++ api/models/database.py
@@ -72,18 +72,40 @@ class DatabaseConnection:
                 self.connection.close()
 
     def insert_user(self, user_name, email, phone_number, password):
+        """
+        insert user details into the table users
+        :param user_name:
+        :param email:
+        :param phone_number:
+        :param password:
+        :return:
+        """
         add_user = """INSERT INTO users (user_name, email, phone_number, password)
                    VALUES ('{0}', '{1}', '{2}', '{3}');""".format(user_name, email, phone_number, password)
         self.cursor.execute(add_user)
         return True
 
     def insert_order(self, order_item, quantity, user_id, item_id):
+        """
+        insert order details into the table orders
+        :param order_item:
+        :param quantity:
+        :param user_id:
+        :param item_id:
+        :return:
+        """
         add_order = """INSERT INTO orders (order_item, quantity, user_id, item_id)
                     VALUES ('{0}', '{1}', '{2}', '{3}');""".format(order_item, quantity, user_id, item_id)
         self.cursor.execute(add_order)
         return True
 
     def insert_menu_item(self, item_name, user_id):
+        """
+        insert menu details into the table menu
+        :param item_name:
+        :param user_id:
+        :return:
+        """
         add_item = "INSERT INTO menu (item_name, user_id) VALUES ('"+item_name+"', '"+user_id+"');"
         self.cursor.execute(add_item)
         return True
@@ -113,18 +135,31 @@ class DatabaseConnection:
         return check_email
 
     def get_menu_items(self):
+        """
+        get all menu items from the menu table
+        :return:
+        """
         menu_items = "SELECT * FROM menu;"
         self.cursor.execute(menu_items)
         menu = self.cursor.fetchall()
         return menu
 
     def get_all_orders(self):
+        """
+        get all orders from the orders table
+        :return:
+        """
         all_orders = "SELECT * FROM orders;"
         self.cursor.execute(all_orders)
         orders = self.cursor.fetchall()
         return orders
 
     def get_a_specific_order(self, order_id):
+        """
+        get a specific order from the orders table using the order_id
+        :param order_id:
+        :return:
+        """
         one = """SELECT * FROM orders WHERE order_id = '{}';""".format(order_id)
         self.cursor.execute(one)
         order = self.cursor.fetchone()
@@ -141,28 +176,32 @@ class DatabaseConnection:
         check_item = self.cursor.fetchone()
         return check_item
 
-    def find_order_by_id(self, order_id):
+    def update_order(self, order_status, order_id):
         """
-        find a specific user given an order id
+        get a specific order using order_id and update order_status
+        :param order_status:
         :param order_id:
         :return:
         """
-        order = """SELECT * FROM orders WHERE order_id = '{}';""".format(order_id)
-        self.cursor.execute(order)
-        check_id = self.cursor.fetchone()
-        return check_id
-
-    def update_order(self, order_status, order_id):
         update = """UPDATE orders SET order_status = '{}' WHERE order_id = '{}';""".format(order_status, order_id)
         self.cursor.execute(update)
 
     def get_order_history(self, user_id):
+        """
+        get the order history for a user from the orders table using the user_id
+        :param user_id:
+        :return:
+        """
         hist = """SELECT * FROM orders WHERE user_id ='{}';""".format(user_id)
         self.cursor.execute(hist)
         get_history = self.cursor.fetchall()
         return get_history
 
     def check_admin(self):
+        """
+        method to set user_type to true which gives a user admin privileges
+        :return:
+        """
         self.cursor.execute("UPDATE users SET user_type = 'TRUE' WHERE user_id = 3")
 
 
